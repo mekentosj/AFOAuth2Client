efe058ed0346bbff1ac75d78d1e31b2113187f09
Ben Blackburne <bpb@lysozyme.net>
2016-06-20 12:46:20 +0100

    Use AFNetworking from Carthage

--- AFOAuth2Client/AFOAuth2Client.h
+++ AFOAuth2Client/AFOAuth2Client.h
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "AFHTTPClient.h"
+#import <AFNetworking/AFNetworking.h>
 
 #ifndef _SECURITY_SECITEM_H_
 #warning Security framework not found in project, or not included in precompiled header. Keychain persistence functionality will not be available.
--- AFOAuth2Client/AFOAuth2Client.m
+++ AFOAuth2Client/AFOAuth2Client.m
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import "AFJSONRequestOperation.h"
+#import <AFNetworking/AFNetworking.h>
 
 #import "AFOAuth2Client.h"
 
